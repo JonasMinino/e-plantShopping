5402bf01d10ba322e68e264bb4c59f18d4b0d25e
jonasminino <jonasminino@gmail.com>
2024-09-07 11:53:35 -0400

    Cart Item

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,45 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
+
+    return cart.reduce((total, item) => total + item.cost * item.quantity, 0).toFixed(2);
  
   };
 
   const handleContinueShopping = (e) => {
+
+     e.preventDefault();
+    if (onContinueShopping) {
+      onContinueShopping();
+    }
    
   };
 
 
 
   const handleIncrement = (item) => {
+
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
+
   };
 
   const handleDecrement = (item) => {
+
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
    
   };
 
   const handleRemove = (item) => {
+
+    dispatch(removeItem(item.name));
+
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+
+    return (item.cost * item.quantity);
+
   };
 
   return (
