327955045e2660b2b74afd15c24c5abcecb6ee04
jonasminino <jonasminino@gmail.com>
2024-09-07 14:02:25 -0400

    debegging 1

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -11,7 +11,7 @@ const CartItem = ({ onContinueShopping }) => {
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
 
-    return cart.reduce((total, item) => total + item.cost * item.quantity, 0).toFixed(2);
+    return cart.reduce((total, item) => total + (item.cost * item.quantity));
  
   };
 
@@ -34,7 +34,9 @@ const CartItem = ({ onContinueShopping }) => {
 
   const handleDecrement = (item) => {
 
+    if(item.quantity > 0){
     dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+    }
    
   };
 
@@ -47,7 +49,7 @@ const CartItem = ({ onContinueShopping }) => {
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
 
-    return (item.cost * item.quantity);
+    return (total + ((parseInt(item.cost.replace('$', '')) * item.quantity)));
 
   };
 
